e03d0115a4cbcb79c3fb9923eb625df2ae3dbd30
Yash <yash100908pratap@gmail.com>
2026-09-21 15:35:40 +0530

    added source code

---
+++ Main.java
@@ -0,0 +1,54 @@
+import java.util.Scanner; // importing scanner
+class Main {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in); // set up new scanner object
+
+        //heading and design
+        System.out.println("CALCULATOR");
+        System.out.println("----------------");
+
+        // declare variables
+        double num1;
+        double num2;
+        char operator;
+        double result = 0;
+
+        // prompt for user input
+        System.out.print("Enter the first number: ");
+        num1 = scanner.nextDouble();
+
+        System.out.print("Choose an operator (+, -, *, /, ^): ");
+        operator = scanner.next().charAt(0);
+
+        System.out.print("Enter the second number: ");
+        num2 = scanner.nextDouble();
+
+        // calculation
+        switch(operator){
+            
+            case '+' : result = num1 + num2;
+            break;
+            
+            case '-' : result = num1 - num2;
+            break;
+            
+            case '*' : result = num1 * num2;
+            break;
+            
+            case '/' : result = num1 / num2;
+            break;
+            
+            case '^' : result = Math.pow(num1, num2);
+            break;
+
+         }
+
+        // result
+         System.out.printf("%.1f %c %.1f is %.2f", num1, operator, num2, result);
+
+        scanner.close(); // close scanner
+
+    }
+}
